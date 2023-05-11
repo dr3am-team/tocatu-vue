fa0f94f6f1b0a42acad4bebab3f21ad4e85eceb6
Pedro Kupersmit <pkupersmit@gmail.com>
2023-05-11 19:41:12 -0300

    add Input component

---
+++ src/components/Input.vue
@@ -0,0 +1,23 @@
+<template>
+  <label for="">
+    {{ this.label }}
+    <input :type="this.type" />
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'number'
+    },
+    label: {
+      type: String,
+      default: 'Label'
+    }
+  }
+}
+</script>
+
+<style></style>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,49 +1,51 @@
 <template>
-    <div>
-        <label for="account-type">Choose your fighter:</label>
-        <select id="account-type" v-model="typeSelected" @click="select"> 
-                <option selected disable hidden>Choose your fighter</option>
-                <option v-for="type in accountType" :key="type.value" :value="type.value">{{ type.label }}</option>
+  <div>
+    <label for="account-type">Choose your fighter:</label>
+    <select id="account-type" v-model="typeSelected" @click="select">
+      <option disabled value="">Choose your fighter</option>
+      <option v-for="type in accountType" :key="type.value" :value="type.value">
+        {{ type.label }}
+      </option>
+    </select>
 
-        </select>
-
-        <div v-if="typeSelected == 'band'">
-            <label for="bandName">Name of your band <input type="text" v-model="band.name"></label>
-            <label for="style">Style of your band <input type="text" v-model="band.style"></label>
-        </div>
-
-        <div v-if="typeSelected == 'bar'">
-             <label for="barName">Name of your bar <input type="text" v-model="bar.name"></label>
-            <label for="style"> Location of your bar <input type="text" v-model="bar.location"></label>
-        </div>
-        <button @click="register">Register</button>
+    <div v-if="typeSelected == 'band'">
+      <label for="bandName">Name of your band <input type="text" v-model="band.name" /></label>
+      <label for="style">Style of your band <input type="text" v-model="band.style" /></label>
     </div>
+
+    <div v-if="typeSelected == 'bar'">
+      <label for="barName">Name of your bar <input type="text" v-model="bar.name" /></label>
+      <label for="style"> Location of your bar <input type="text" v-model="bar.location" /></label>
+    </div>
+    <button @click="register">Register</button>
+  </div>
 </template>
 
 <script>
+import Input from '../components/Input.vue'
 export default {
-    components: {},
-    setup() {
-
-    },
-    data() {
-        return {
-            accountType: [{ label: 'Band', value: 'band' }, { label: 'Bar', value: 'bar' }],
-            typeSelected: '',
-            band: {name: '', style: ''},
-            bar: {name: '', location: ''}
-        }
-    },
-    methods: {
-        register() {
-            //TODO
-        },
-        select(e) {
-            this.typeSelected = e.target.value
-        }
+  components: { Input },
+  setup() {},
+  data() {
+    return {
+      accountType: [
+        { label: 'Band', value: 'band' },
+        { label: 'Bar', value: 'bar' }
+      ],
+      typeSelected: '',
+      band: { name: '', style: '' },
+      bar: { name: '', location: '' }
     }
+  },
+  methods: {
+    register() {
+      //TODO
+    },
+    select(e) {
+      this.typeSelected = e.target.value
+    }
+  }
 }
 </script>
-  
+
 <style></style>
-  
