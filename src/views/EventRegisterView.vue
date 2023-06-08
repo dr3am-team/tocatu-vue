216cf4de46ca98605796973c6489e3889e375506
flerner <felix.lerner01@gmail.com>
2023-06-07 21:48:22 -0300

    menos setenta

--- src/views/EventRegisterView.vue
+++ src/views/EventRegisterView.vue
@@ -88,7 +88,7 @@ export default {
   }
 }
 .super-container {
-  min-height: calc(100vh - 51px);
+  min-height: calc(100vh - 70px);
   display: flex;
   flex-direction: column;
   justify-content: space-between;
